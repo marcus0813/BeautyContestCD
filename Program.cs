9d7d160713586aa8ff824a9a1db32131cc595df3
Rotiboy95 <polonium209@live.com>
2023-02-01 19:56:51 +0800

    First Commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using API.Extensions;
+using API.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
+
+app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
