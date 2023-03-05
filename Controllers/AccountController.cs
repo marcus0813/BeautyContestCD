b52ae353b45072d6cc345c734d2d298c74354b95
Rotiboy95 <polonium209@live.com>
2023-03-05 09:50:10 +0800

    CICD fixes #10

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -57,7 +57,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _userManager.Users
+            var user = await _userManager?.Users
             .Include(p => p.Photos)
             .SingleOrDefaultAsync(x =>
                     x.UserName == loginDto.Username);
